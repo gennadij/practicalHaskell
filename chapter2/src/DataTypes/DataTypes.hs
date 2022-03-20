b8405816bb2b3eed94f3ff82fc6c79ad0870c5b2
Gennadi <gennadiheimann@gmail.com>
2022-03-20 11:31:20 +0100

    add chapters

--- chapter2/src/DataTypes/DataTypes.hs
+++ chapter2/src/DataTypes/DataTypes.hs
@@ -21,13 +21,18 @@ type Price = Double
 data TimeMachine = TimeMachine Manufacturer Model TMName Price deriving Show
 
 data ClientR = GovOrgR     {   clientRName :: String     }
-
              | CompanyR    {   clientRName :: String
                              , companyRId  :: Integer
                              , personR     :: PersonR
                              , positionR   :: String     }
-
              | IndividualR {   personR      :: PersonR    } deriving Show
 
 data PersonR = PersonR     {   fNameR       :: String
                              , lNameR       :: String     } deriving Show
+
+class Nameable n where
+  name :: n -> String 
+
+instance Nameable (ClientR i) where 
+  name IndividualR {personR = PersonR{fNameR = f, lNameR = n}} = f ++ " " ++ name
+  name c = clientRName c
