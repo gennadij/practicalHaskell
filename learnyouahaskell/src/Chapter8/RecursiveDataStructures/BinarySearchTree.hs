f4e1073b60b89be3d3acec813191b7eb7f5e48f2
Gennadi <gennadiheimann@gmail.com>
2022-04-17 16:45:13 +0200

    Binary tree second test

--- learnyouahaskell/src/Chapter8/RecursiveDataStructures/BinarySearchTree.hs
+++ learnyouahaskell/src/Chapter8/RecursiveDataStructures/BinarySearchTree.hs
@@ -8,11 +8,10 @@ singelton x = Node x EmptyTree EmptyTree
 
 treeInsert :: (Ord a) => a -> Tree a -> Tree a
 treeInsert x EmptyTree = singelton x
-treeInsert x (Node a left right)
+treeInsert x (Node a left right) 
   | x == a = Node x left right
   | x <  a = Node x (treeInsert x left) right
   | x >  a = Node x left (treeInsert x right)
-  | otherwise = singelton x
 
 treeElem :: (Ord a) => a -> Tree a -> Bool
 treeElem x emptyTree = False 
@@ -20,3 +19,13 @@ treeElem x (Node a left right)
   | x == a = True 
   | x <  a = treeElem x left 
   | x >  a = treeElem x right
+
+data Tree1 a = Empty
+             | Leaf a
+             | Branch a (Tree1 a) (Tree1 a)
+             deriving (Eq, Show)
+
+values :: Tree1 a -> [a]
+values Empty = []
+values (Leaf x) = [x]
+values (Branch x l r) = values l ++ [x] ++ values r
