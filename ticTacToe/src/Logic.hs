e7e3644612d3b0294538b1a04d27fb394ef8cabc
Gennadi <gennadiheimann@gmail.com>
2022-05-12 23:43:40 +0200

    update TicTacToe

--- ticTacToe/src/Logic.hs
+++ ticTacToe/src/Logic.hs
@@ -1,7 +1,8 @@
 module Logic where 
 
 import Data.Array as A
-import System.Random (StdGen)
+import System.Random (StdGen, randomR)
+import Control.Monad.State
 
 data GameState = GameState 
   {
@@ -15,3 +16,22 @@ data Player = XPlayer | OPlayer
 data TileState = Empty | HasX | HasO deriving Eq
 
 type TileIndex = (Int, Int)
+
+chooseRandomMove :: State GameState TileIndex
+chooseRandomMove = do 
+  game <- get
+  let openSpots = [fst pair | pair <- A.assocs (board game), snd pair == Empty]
+  let gen = generator game
+  let (i, gen') = randomR (0, length openSpots - 1) gen
+  put $ game {generator = gen'}
+  return $ openSpots !! i
+
+applyMove :: TileIndex -> State GameState ()
+applyMove i = do
+  game <- get 
+  let p = currentPlayer game
+  let newBoard = board game A.// [(i, tileForPlayer p)]
+
+  tileForPlayer :: Player -> TileState
+  tileForPlayer XPlayer = HasX
+  tileForPlayer OPlayer = HasO
