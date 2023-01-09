bda70c4589fbbfb81b4180737958178eaa066268
Gennadi <gennadiheimann@gmail.com>
2023-01-09 22:05:47 +0100

    chapter6 kMean main function

--- practicalHaskell/app/Main.hs
+++ practicalHaskell/app/Main.hs
@@ -2,11 +2,14 @@ module Main (main) where
 
 import Chapter2
 import Chapter3
+import Chapter6
 
 main :: IO ()
 main = do
   --testFoldr
   --testFoldl
   --testInfFoldr
-  print (duplicateOdds [1,2,3,4,5,6,7,8,9])
+  -- print (duplicateOdds [1,2,3,4,5,6,7,8,9])
   -- print ( permutationsStartWith 'A' "DTsngzswarakljdlAölkjAkjfasdjfls")
+  let info = [(1, 1), (1, 2), (4, 4), (4, 5), (8, 3), (3, 8), (4, 5), (10, 1)] :: [(Double, Double)]
+  print $ kMeans initializeSample 2 info 0.001
--- practicalHaskell/src/Chapter6.hs
+++ practicalHaskell/src/Chapter6.hs
@@ -34,4 +34,27 @@ newCentroidPhase :: (Vector v, Vectorizable e v) => M.Map v [e] -> [(v, v)]
 newCentroidPhase = M.toList . fmap (centroid . map toVector)
 
 shouldStop :: (Vector v) => [(v, v)] -> Double -> Bool
-shouldStop centroids threshold = foldr (\(x, y) s -> s + distance x y) 0.0 centroids < threshold 
+shouldStop centroids threshold = foldr (\(x, y) s -> s + distance x y) 0.0 centroids < threshold
+
+kMeans :: (Vector v, Vectorizable e v) 
+  => (Int -> [e] -> [v]) -- initialization function
+  -> Int                 -- number of centroids
+  -> [e]                 -- the information
+  -> Double              -- threshold
+  -> [v]                 -- final centroids
+kMeans i k points = kMeans' (i k points) points
+
+kMeans' :: (Vector v, Vectorizable e v) =>
+  [v] -> [e] -> Double -> [v]
+kMeans' centroids points threshold = 
+  let assignments     = clusterAssignmentPhase centroids points
+      oldNewCentroids = newCentroidPhase assignments
+      newCentroids    = map snd oldNewCentroids
+  in if shouldStop oldNewCentroids threshold
+     then newCentroids
+     else kMeans' newCentroids points threshold
+     
+initializeSample :: Int -> [e] -> [(Double,Double)]
+initializeSample 0 _ = []
+initializeSample n v = (fromIntegral n, fromIntegral n) : initializeSample (n - 1) v
+
