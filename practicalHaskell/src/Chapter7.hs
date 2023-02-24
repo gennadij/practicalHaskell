1bec9bf9fe7e2e7862491b5b726d030944df0d90
Gennadi Heimann <gennadiheimann@gmail.com>
2023-02-24 12:26:09 +0100

    Chapter7

--- practicalHaskell/src/Chapter7.hs
+++ practicalHaskell/src/Chapter7.hs
@@ -1,2 +1,14 @@
 module Chapter7 where
 
+import Control.Monad (replicateM)
+
+
+brockenThreeJumps :: Int  -> [Int]
+brockenThreeJumps wishYear = do
+  jump1 <- [-1, 3, 5]
+  jump2 <- [-1, 3, 5]
+  jump3 <- [-1, 3, 5]
+  return $ wishYear + jump1 + jump2 + jump3
+
+brockenJumps :: Int -> Int -> [[Int]]
+brockenJumps year jumps = replicateM jumps [-1, 3 , 5]
