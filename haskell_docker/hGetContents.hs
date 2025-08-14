f7409eb1b7fc1ae90210e763e0470cca7ea30462
Gennadi Heimann <genaheimann@gmail.com>
2025-08-14 08:41:38 +0000

    extract and removes line

--- haskell_docker/hGetContents.hs
+++ haskell_docker/hGetContents.hs
@@ -2,23 +2,32 @@ import System.IO
 import Data.List.Split
 
 main = do
-    withFile "test.csv" ReadMode (
-        \handle -> do
-            contents <- hGetContents handle
-            let linesOfContents = lines contents
+    contents <- readFile "test.csv"  
+    let linesOfContents = lines contents
             --    zipLinesOfContents = zipWith(\n l -> show n ++ " " ++ l) [1..] linesOfContents
-            --mapM putStr (extactLine (unlines linesOfContents))
-            return ()
-        )
+    mapM putStrLn (linesOfContents)
+
+getTestString :: [String]
+getTestString = ["test1;test2;test3;","test4;test5;test6;"]
 
 extactLine :: String -> [String]
 extactLine l = endBy ";" l
 
--- extractLinesFromFiles :: String -> [[String]]
--- extractLinesFromFiles filePath = do 
---     withFile filePath ReadMode (
---         \handle -> do
---            contents <- hGetContents handle
---            let linesOfContents = lines contents
---            return linesOfContents
---        )
+extractLines :: [[String]]
+extractLines = map extactLine getTestString 
+
+extractLinesFromFile :: String -> IO [[String]]
+extractLinesFromFile fileName = do
+    contents <- readFile fileName
+    let linesOfContents = lines contents
+        extractedLines = map extactLine linesOfContents
+        removeAtPositions = dropPCollumsAtPositions [0, 2] extractedLines
+    return removeAtPositions
+
+dropPCollumsAtPositions :: [Int] ->  [[String]] -> [[String]]
+dropPCollumsAtPositions positions rows = map (removePositions positions) rows
+    where
+        removePositions pos row = [x | (i, x) <- zip [0..] row, not (i `elem` pos)]
+
+-- collectCSV :: [[String]]-> [String]
+-- collectCSV :: 
