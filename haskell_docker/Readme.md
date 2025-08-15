b2fc6f64157a6c3d8996234ec38178816e8fe880
Gennadi Heimann <bertrandt.heimann@extaccount.com>
2025-08-15 08:18:17 +0000

    impl remve rows

--- haskell_docker/Readme.md
+++ haskell_docker/Readme.md
@@ -2,4 +2,4 @@
 `cabal install --lib --package-env . split`
 
 ### Beispiel für DKB Auszug/Export CSV
-`/updateCsv -f test2.csv -o updated2.csv --to-delete -c 0,2,3,4,6,7,9,10,11 -r 1`
+`./updateCsv -f test2.csv -o updated2.csv --to-delete -c 0,2,3,4,6,7,9,10,11 -r 0,1`
--- haskell_docker/updateCsv.hs
+++ haskell_docker/updateCsv.hs
@@ -18,9 +18,8 @@ main = do
         arg_to_delete = filter (== "--to-delete") args
         arg_c = filter (== "-c") args
         args_columnsToDelete = if null arg_c then [] else splitOn "," (args !! 6)
-        -- TODO (gh) not yet imlemented
-        -- arg_r = filter (== "-r") args
-        -- args_rowsToDelete = if null arg_r then [] else splitOn "," (args !! 8)
+        arg_r = filter (== "-r") args
+        args_rowsToDelete = if null arg_r then [] else splitOn "," (args !! 8)
     -- putStrLn $ head arg_f
     -- putStrLn arg_fileName
     -- putStrLn $ head  arg_o
@@ -30,45 +29,49 @@ main = do
     -- mapM putStrLn $ args_columnsToDelete
     -- putStrLn $ head arg_r
     -- mapM putStrLn $ args_rowsToDelete
-    updateCsv arg_fileName arg_outputFileName (map read args_columnsToDelete :: [Int])
+    updateCsv arg_fileName arg_outputFileName (convertStringToIntList  args_columnsToDelete) (convertStringToIntList args_rowsToDelete)
+        where convertStringToIntList xs = map read xs :: [Int]
 
-
-getTestString :: [String]
-getTestString = ["test1;test2;test3;","test4;test5;test6;"]
-
-extactLine :: String -> [String]
-extactLine l = endBy ";" l
-
-extractLines :: [[String]]
-extractLines = map extactLine getTestString 
-
-updateCsv :: InFile -> OutFile -> ColumnsToDelete -> IO ()
-updateCsv inFile outFile columnsToDelete = do
+updateCsv :: InFile -> OutFile -> ColumnsToDelete -> RowsToDelete ->  IO ()
+updateCsv inFile outFile columnsToDelete rowsToDelete = do
     contents <- readFile inFile
     let linesOfContents = lines contents
         extractedLines = map extactLine linesOfContents
-        removeAtPositions = dropPCollumsAtPositions columnsToDelete extractedLines
-        collectedNewCSV = collectCSV removeAtPositions
+        removedRowsAtPositions = dropRowsAtPositions rowsToDelete extractedLines
+        removedColumsAtPositions = dropCollumsAtPositions columnsToDelete removedRowsAtPositions
+        collectedNewCSV = collectCSV removedColumsAtPositions
     writeFile outFile collectedNewCSV
 
-
-extractLinesFromFile :: String -> IO ()
-extractLinesFromFile fileName = do
-    contents <- readFile fileName
-    let linesOfContents = lines contents
-        extractedLines = map extactLine linesOfContents
-        removeAtPositions = dropPCollumsAtPositions [1] extractedLines
-        collectedNewCSV = collectCSV removeAtPositions
-    writeFile "updated.csv" collectedNewCSV
-
-dropPCollumsAtPositions :: [Int] ->  [[String]] -> [[String]]
-dropPCollumsAtPositions positions rows = map (removePositions positions) rows
+dropCollumsAtPositions :: [Int] ->  [[String]] -> [[String]]
+dropCollumsAtPositions positions rows = map (removePositions positions) rows
     where
         removePositions pos row = [x | (i, x) <- zip [0..] row, not (i `elem` pos)]
+
+dropRowsAtPositions :: [Int] -> [[String]] -> [[String]]
+dropRowsAtPositions positions rows = [x | (i, x) <- zip [0..] rows, not (i `elem` positions)]
 
 collectCSV :: [[String]] -> String 
 collectCSV xs = addNewLine ( map addSimicolon xs )
     where
         addSimicolon xs = concatMap (++ ";") xs
         addNewLine xs = concatMap (++ "\n") xs
+
+extactLine :: String -> [String]
+extactLine l = endBy ";" l
+
+----------------------------------------------------------------------------
+extractLinesFromFile :: String -> IO ()
+extractLinesFromFile fileName = do
+    contents <- readFile fileName
+    let linesOfContents = lines contents
+        extractedLines = map extactLine linesOfContents
+        removeAtPositions = dropCollumsAtPositions [1] extractedLines
+        collectedNewCSV = collectCSV removeAtPositions
+    writeFile "updated.csv" collectedNewCSV
+
+getTestString :: [String]
+getTestString = ["test1;test2;test3;","test4;test5;test6;"]
+
+extractLines :: [[String]]
+extractLines = map extactLine getTestString 
         
