8b3d6b6d40ce5885ae3ef63e4e4610562bc47437
Gennadi Heimann <bertrandt.heimann@extaccount.com>
2025-08-15 06:59:07 +0000

    working tool

--- haskell_docker/Readme.md
+++ haskell_docker/Readme.md
@@ -1,2 +1,5 @@
 ### Install split lib
 `cabal install --lib --package-env . split`
+
+### Beispiel für DKB Auszug/Export CSV
+`/updateCsv -f test2.csv -o updated2.csv --to-delete -c 0,2,3,4,6,7,9,10,11 -r 1`
---
+++ haskell_docker/updateCsv.hs
@@ -0,0 +1,74 @@
+import System.IO
+import Data.List.Split
+import System.Environment (getArgs)
+
+-- updateCsv -f test.csv -o updated.csv --to-delete -c 1,4,5 -r 1,5,6
+type InFile = String
+type OutFile = String
+type ColumnsToDelete = [Int]
+type RowsToDelete = [Int]
+
+main = do
+    args <- getArgs
+    -- TODO (gh) check if missing parameters
+    let arg_f = filter (== "-f") args
+        arg_fileName = if null arg_f then "" else args !! 1
+        arg_o = filter (== "-o") args
+        arg_outputFileName = if null arg_o then "" else args !! 3
+        arg_to_delete = filter (== "--to-delete") args
+        arg_c = filter (== "-c") args
+        args_columnsToDelete = if null arg_c then [] else splitOn "," (args !! 6)
+        -- TODO (gh) not yet imlemented
+        -- arg_r = filter (== "-r") args
+        -- args_rowsToDelete = if null arg_r then [] else splitOn "," (args !! 8)
+    -- putStrLn $ head arg_f
+    -- putStrLn arg_fileName
+    -- putStrLn $ head  arg_o
+    -- putStrLn $ arg_outputFileName
+    -- putStrLn $ head arg_to_delete
+    -- putStrLn $ head arg_c
+    -- mapM putStrLn $ args_columnsToDelete
+    -- putStrLn $ head arg_r
+    -- mapM putStrLn $ args_rowsToDelete
+    updateCsv arg_fileName arg_outputFileName (map read args_columnsToDelete :: [Int])
+
+
+getTestString :: [String]
+getTestString = ["test1;test2;test3;","test4;test5;test6;"]
+
+extactLine :: String -> [String]
+extactLine l = endBy ";" l
+
+extractLines :: [[String]]
+extractLines = map extactLine getTestString 
+
+updateCsv :: InFile -> OutFile -> ColumnsToDelete -> IO ()
+updateCsv inFile outFile columnsToDelete = do
+    contents <- readFile inFile
+    let linesOfContents = lines contents
+        extractedLines = map extactLine linesOfContents
+        removeAtPositions = dropPCollumsAtPositions columnsToDelete extractedLines
+        collectedNewCSV = collectCSV removeAtPositions
+    writeFile outFile collectedNewCSV
+
+
+extractLinesFromFile :: String -> IO ()
+extractLinesFromFile fileName = do
+    contents <- readFile fileName
+    let linesOfContents = lines contents
+        extractedLines = map extactLine linesOfContents
+        removeAtPositions = dropPCollumsAtPositions [1] extractedLines
+        collectedNewCSV = collectCSV removeAtPositions
+    writeFile "updated.csv" collectedNewCSV
+
+dropPCollumsAtPositions :: [Int] ->  [[String]] -> [[String]]
+dropPCollumsAtPositions positions rows = map (removePositions positions) rows
+    where
+        removePositions pos row = [x | (i, x) <- zip [0..] row, not (i `elem` pos)]
+
+collectCSV :: [[String]] -> String 
+collectCSV xs = addNewLine ( map addSimicolon xs )
+    where
+        addSimicolon xs = concatMap (++ ";") xs
+        addNewLine xs = concatMap (++ "\n") xs
+        
